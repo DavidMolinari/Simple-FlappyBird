bc60eb10933a9c2847141209c6d47524d0e1344d
David Molinari <contact@davidmolinari.fr>
2016-04-15 01:39:44 +0200

    Add JFrame, fonctionnel

--- src/flappyBird/FlappyBird.java
+++ src/flappyBird/FlappyBird.java
@@ -1,2 +1,25 @@
 package flappyBird;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class FlappyBird {
+	
+	public final int WIDTH = 800, HEIGHT = 600;
+	
+	public static FlappyBird flappyBird;
+	
+	public FlappyBird(){
+		
+		JFrame jframe = new JFrame();
+		jframe.setSize(WIDTH, HEIGHT);
+		jframe.setVisible(true);
+		jframe.setResizable(false);
+		jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
+	}
+	public static void main(String[] args) {
+		flappyBird = new FlappyBird();
+		
+	}
+}
