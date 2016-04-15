e755cce266d99ac77f6e91955f2030422afcd6d9
David Molinari <contact@davidmolinari.fr>
2016-04-15 02:25:59 +0200

    Columns base

--- src/flappyBird/FlappyBird.java
+++ src/flappyBird/FlappyBird.java
@@ -5,51 +5,89 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
-public class FlappyBird implements ActionListener {
+public class FlappyBird implements ActionListener
+{
 	
 	public final int WIDTH = 800, HEIGHT = 600;
 	
 	public static FlappyBird flappyBird;
 	
 	public Renderer renderer;
+	public Random rand;
 	
 	public Rectangle minou;
 	
-	public FlappyBird(){
+	// Colletion de rectangles pour les colonnes.
+	public ArrayList<Rectangle> columns;
+	public FlappyBird()
+	{
 		
 		
 		JFrame jframe = new JFrame();
 		Timer timer = new Timer(20, this);
 		
 		
-		jframe.setTitle("Flappy Bird");
 		renderer = new Renderer();
+		rand = new Random();
+		
 		jframe.add(renderer);
+		jframe.setTitle("Flappy Bird");
+
 		jframe.setSize(WIDTH, HEIGHT);
 		jframe.setVisible(true);
 		jframe.setResizable(false);
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		minou = new Rectangle(WIDTH / 2 -10, HEIGHT / 2 -10, 20, 20);
+		columns = new ArrayList<Rectangle>();
+		addColumn(true);
 		timer.start();
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		flappyBird = new FlappyBird();
 		
 	}
 	
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent e)
+	{
 		renderer.repaint();
 	}
 	
+	public void addColumn( boolean start)
+	{
+		int space = 300; // Espace entre les colonnes
+		int width = 100;
+		int height = 50 + rand.nextInt(300);
+		
+		if(start)
+		{
+			columns.add(new Rectangle(WIDTH + width + columns.size() * 300, HEIGHT - height - 120, width, height));
+			columns.add(new Rectangle(WIDTH + width + (columns.size() - 1 * 300), 0, width, HEIGHT - height - space));
+		}
+		else 
+		{
+			columns.add(new Rectangle(columns.get(columns.size() - 1).x + 600, HEIGHT - height - 120, width, height));
+			columns.add(new Rectangle(columns.get(columns.size() - 1).x,0, width, HEIGHT - height - space));
+		}
 	
-	public void repaint(Graphics g) {
+	}
+	
+	public void paintColumn(Graphics g, Rectangle column)
+	{
+		g.setColor(Color.green.darker());
+		g.fillRect(column.x, column.y, column.width, column.height);
+	}
+	
+	public void repaint(Graphics g) 
+	{
 		// TODO Auto-generated method stub
 		
 		//Background
